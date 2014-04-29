ffd077ef74f72d5b212dad6c37e605264f14f049
Tri Nguyen <tri@tridnguyen.com>
2014-04-29 11:41:20 -0400

    add maps

---
+++ states.js
@@ -0,0 +1,60 @@
+var states = [
+  {
+    fips: "36",
+    name: "New York",
+    code: "NY"
+  },
+  {
+    fips: "06",
+    name: "California",
+    code: "CA"
+  }
+];
+
+var movies = {
+  "CA": {
+    numTheaters: 40
+  },
+  "NY": {
+    numTheaters: 50
+  }
+};
+
+var stateMap = new Landline.Stateline('#states-map', 'states');
+
+var tooltipTmpl = _.template($('#state-tooltip-tmpl').html());
+
+// Add tooltips, and cache the existing style
+// to put it back in place on mouseout
+stateMap.on('mouseover', function(e, path, data) {
+  data.existingStyle = (data.existingStyle || {});
+  data.existingStyle["fill"]        = path.attr("fill");
+  data.existingStyle["strokeWidth"] = path.attr("stroke-width");
+  path.attr("fill", "#999").attr("stroke-width", 1);
+});
+
+stateMap.on('mousemove', function(e, path, data) {
+  var state = _(states).findWhere({fips: data.fips});
+  if (state) {
+    $("#state-tooltip").html(tooltipTmpl({
+      name: state.name,
+      numTheaters: movies[state.code].numTheaters
+    })).css("left", e.pageX + 20).css("top", e.pageY + 20).show();
+  }
+});
+
+stateMap.on('mouseout', function(e, path, data) {
+  $("#state-tooltip").hide();
+  _(data.existingStyle).each(function(value, attribute) {
+    path.attr(attribute, value);
+  });
+});
+
+_(movies).each(function(value, stateName) {
+  var state = _(states).findWhere({code: stateName});
+  stateMap.style(state.fips, 'fill', 'rgb(0,109,50)');
+});
+
+stateMap.createMap();
+
+console.log(stateMap);
